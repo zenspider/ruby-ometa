ec8d32a1b0881066e92d7b6406c0ba2a2d29f894
Ryan Davis <rdavis@engineyard.com>
2008-10-23 09:31:46 -0500

    Reformatted nearly everything... TABS?!?! wtf?

--- test/test_ometa.rb
+++ test/test_ometa.rb
@@ -8,42 +8,42 @@ __END__
 # as an example (this is missing the optimization pass):
 #
 class Ometa
-	def self.Grammar(filename)
-		grammar = File.read(filename)
-		ast = OMetaParser.parsewith(grammar, 'grammar')
-		ruby = RubyOMetaTranslator.matchwith(ast, 'trans')
-		#ruby = File.read('_debug.rb')
-		begin
-			open('_debug.rb', 'w') { |f| f << ruby }
-			eval ruby
-		rescue SyntaxError
-			puts '* error compiling grammar'
-			puts '* ast:'
-			require 'pp'
-			pp ast
-			puts '* ruby:'
-			puts ruby
-			open('_debug.rb', 'w') { |f| f << ruby }
-			raise
-		end
-	end
+  def self.Grammar(filename)
+    grammar = File.read(filename)
+    ast = OMetaParser.parsewith(grammar, 'grammar')
+    ruby = RubyOMetaTranslator.matchwith(ast, 'trans')
+    #ruby = File.read('_debug.rb')
+    begin
+      open('_debug.rb', 'w') { |f| f << ruby }
+      eval ruby
+    rescue SyntaxError
+      puts '* error compiling grammar'
+      puts '* ast:'
+      require 'pp'
+      pp ast
+      puts '* ruby:'
+      puts ruby
+      open('_debug.rb', 'w') { |f| f << ruby }
+      raise
+    end
+  end
 end
 
 class Calc < Ometa::Grammar('calc.ometa')
-	def self.calc str
-		matchAllwith str, 'expr'
-	end
+  def self.calc str
+    matchAllwith str, 'expr'
+  end
 end
 
 class JSParser < Ometa::Grammar('js_parser.ometa')
-	KEYWORDS = ["break", "case", "catch", "continue", "default", "delete", "do", "else", "finally", "for", "function", "if", "in",
-            "instanceof", "new", "return", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "ometa"]
+  KEYWORDS = ["break", "case", "catch", "continue", "default", "delete", "do", "else", "finally", "for", "function", "if", "in",
+              "instanceof", "new", "return", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "ometa"]
 
-	KEYWORDS_HASH = KEYWORDS.inject({}) { |h, k| h.update k => true }
+  KEYWORDS_HASH = KEYWORDS.inject({}) { |h, k| h.update k => true }
 
-	def self._isKeyword k
-		KEYWORDS_HASH[k.to_s]
-	end
+  def self._isKeyword k
+    KEYWORDS_HASH[k.to_s]
+  end
 end
 
 p JSParser.parsewith("  x  += 1;\n", 'expr')
